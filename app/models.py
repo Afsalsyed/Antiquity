1560bc6b63eda29a722838da3866fd9aa5c5f26e
Afsalsyed <afsalsyed12@gmail.com>
2024-07-02 10:04:23 +0530

    connected to DB

--- app/models.py
+++ app/models.py
@@ -4,11 +4,11 @@ from django.db import models # type: ignore
 class Product(models.Model):
 
     name = models.CharField(max_length=150)    
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     description = models.TextField()
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    end_time = models.DateTimeField()
+    created_at = models.DateField(auto_now_add=True)
+    end_time = models.DateField()
 
     def __str__(self):
         return self.name
